2b105e27a517d846b8aa8d9c1a014303d4ce02d9
Travis Woods <woodstr3313@gmail.com>
2022-05-05 17:58:02 -0400

    started updating routes

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,13 +5,25 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+  Product.belongsTo(Category, {
+    foreignKey: "category_id"
+  })
 
 // Categories have many Products
+  Category.hasMany(Product, {
+    foreignKey: "category_id"
+  })
 
 // Products belongToMany Tags (through ProductTag)
-
+  Products.belongToMany(Tag, {
+    foreignKey: "product_id",
+    through: ProductTag
+  })
 // Tags belongToMany Products (through ProductTag)
-
+  Tag.belongToMany(Product, {
+    foreignKey: "tag_id",
+    through: ProductTag,
+  })
 module.exports = {
   Product,
   Category,
--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,6 +6,11 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: [Product]
+  })
+  .then((categories) => res.json(categories))
+  .catch((err)=> res.status(500).json(err))
 });
 
 router.get('/:id', (req, res) => {
